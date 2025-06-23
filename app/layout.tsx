887db87a0fd249215311868fa0d81d49253e16aa
watataku8911 <watano.takuya@gmail.com>
2025-06-23 15:24:53 +0900

    エラーも解消

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="ja" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
--- components/Theme.tsx
+++ components/Theme.tsx
@@ -2,16 +2,30 @@
 
 import { useTheme } from "next-themes";
 import Switch from "./Switch";
+import Toggle from "./Toggle";
+import { useEffect, useState } from "react";
 
 const Theme = () => {
   const { theme, setTheme } = useTheme();
-  if (theme === undefined) return null;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
 
   return (
-    <Switch
-      darkTheme={theme}
-      handleToggle={() => setTheme(theme === "dark" ? "light" : "dark")}
-    />
+    <>
+      <Switch
+        darkTheme={theme}
+        handleToggle={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
+      <Toggle
+        darkTheme={theme}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      />
+    </>
   );
 };
 
